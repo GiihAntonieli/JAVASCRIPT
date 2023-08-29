e6122205342bd95821766bb6ff082b46f5a88c83
Giovanna Antonieli <129458706+GiihAntonieli@users.noreply.github.com>
2023-08-29 14:38:55 -0300

    ex016

---
+++ aula14ex/ex016/mod1.js
@@ -0,0 +1,13 @@
+function contar(){
+    let inicio =  document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+
+    if (inicio.ariaValueMax.length == 0 || fim.ariaValueMax.length == 0 || passo.ariaValueMax.legth == 0){
+        window.alert('[ERRO] Faltam dados!')
+    }
+    else if(){
+        window.alert('Tudo ok!')
+    }
+}
+
